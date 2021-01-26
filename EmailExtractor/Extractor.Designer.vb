a1f54e555eb2c55dc986c6b94cd2018425ca82a0
GUSTAVO GERMINIANI <gugart@hotmail.com>
2021-01-26 15:22:16 -0300

    Adicionar arquivos de projeto.

---
+++ EmailExtractor/Extractor.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Email
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Email))
+        Me.Emails = New System.Windows.Forms.DataGridView()
+        Me.Extractor = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ExeBTN = New System.Windows.Forms.Button()
+        Me.ExportBTN = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.E_mails = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BackgroundWorker = New System.ComponentModel.BackgroundWorker()
+        Me.CancBTN = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.Emails, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Emails
+        '
+        Me.Emails.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Extractor})
+        Me.Emails.Location = New System.Drawing.Point(12, 41)
+        Me.Emails.Name = "Emails"
+        Me.Emails.RowHeadersVisible = False
+        Me.Emails.Size = New System.Drawing.Size(640, 356)
+        Me.Emails.TabIndex = 6
+        '
+        'Extractor
+        '
+        Me.Extractor.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.Extractor.HeaderText = "E-mail Extractor"
+        Me.Extractor.Name = "Extractor"
+        '
+        'ExeBTN
+        '
+        Me.ExeBTN.Location = New System.Drawing.Point(12, 12)
+        Me.ExeBTN.Name = "ExeBTN"
+        Me.ExeBTN.Size = New System.Drawing.Size(75, 23)
+        Me.ExeBTN.TabIndex = 4
+        Me.ExeBTN.Text = "Executar"
+        Me.ExeBTN.UseVisualStyleBackColor = True
+        '
+        'ExportBTN
+        '
+        Me.ExportBTN.Enabled = False
+        Me.ExportBTN.Location = New System.Drawing.Point(554, 12)
+        Me.ExportBTN.Name = "ExportBTN"
+        Me.ExportBTN.Size = New System.Drawing.Size(98, 23)
+        Me.ExportBTN.TabIndex = 5
+        Me.ExportBTN.Text = "Exportar CSV"
+        Me.ExportBTN.UseVisualStyleBackColor = True
+        '
+        'E_mails
+        '
+        Me.E_mails.Name = "E_mails"
+        '
+        'BackgroundWorker
+        '
+        Me.BackgroundWorker.WorkerReportsProgress = True
+        Me.BackgroundWorker.WorkerSupportsCancellation = True
+        '
+        'CancBTN
+        '
+        Me.CancBTN.Location = New System.Drawing.Point(473, 12)
+        Me.CancBTN.Name = "CancBTN"
+        Me.CancBTN.Size = New System.Drawing.Size(75, 23)
+        Me.CancBTN.TabIndex = 8
+        Me.CancBTN.Text = "Cancelar"
+        Me.CancBTN.UseVisualStyleBackColor = True
+        Me.CancBTN.Visible = False
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripProgressBar1, Me.ToolStripStatusLabel2})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 400)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(664, 22)
+        Me.StatusStrip1.TabIndex = 10
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.ForeColor = System.Drawing.Color.Red
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(0, 17)
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(200, 16)
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.ForeColor = System.Drawing.Color.Red
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(0, 17)
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Email
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(664, 422)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.CancBTN)
+        Me.Controls.Add(Me.ExportBTN)
+        Me.Controls.Add(Me.ExeBTN)
+        Me.Controls.Add(Me.Emails)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Email"
+        Me.Text = "E-mail Extractor"
+        CType(Me.Emails, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Emails As System.Windows.Forms.DataGridView
+    Friend WithEvents ExeBTN As System.Windows.Forms.Button
+    Friend WithEvents ExportBTN As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents E_mails As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Extractor As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents BackgroundWorker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents CancBTN As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripProgressBar1 As System.Windows.Forms.ToolStripProgressBar
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ EmailExtractor/Extractor.vb
@@ -0,0 +1,321 @@
+﻿Imports System.ComponentModel
+Imports Microsoft.Office.Interop.Outlook
+Imports Microsoft.Office.Interop
+Imports System.Text.RegularExpressions
+
+Public Class Email
+    Dim ProcessoFinalizado As Boolean
+    Dim Retorno As New List(Of String)()
+    Dim Itens As Integer
+    Dim CalculaTempo As Integer
+    Dim Contador As Integer
+    Dim calc As Integer = 0
+
+    'Private Sub AbrirBTN_Click(sender As Object, e As EventArgs)
+
+    '    Procurar.ShowDialog()
+    '    ArquivoTXT.Text = Procurar.FileName
+
+    'End Sub
+
+    'Private Sub RunBTN_Click(sender As Object, e As EventArgs)
+
+    '    If ArquivoTXT.Text = "" Then
+    '        MsgBox("Selecione o arquivo a ser executado.", MsgBoxStyle.Critical, "Erro")
+    '        Exit Sub
+    '    Else
+    '        RUN_pst()
+    '    End If
+
+    'End Sub
+
+    'Private Sub RUN_pst()
+    '    Dim file As String = IO.Path.GetFileNameWithoutExtension(Procurar.FileName)
+    '    Try
+    '        Dim mailItems As IEnumerable(Of MailItem) = readPst(ArquivoTXT.Text, file)
+    '        For Each mailItem As MailItem In mailItems
+    '            Console.WriteLine(mailItem.SenderName + " - " + mailItem.Subject)
+    '            DataGridView1.Rows.Add(mailItem.SenderName + " - " + mailItem.Subject)
+    '        Next
+    '    Catch ex As System.Exception
+    '        Console.WriteLine(ex.Message)
+    '    End Try
+    '    Console.ReadLine()
+    'End Sub
+
+    'Private Shared Function readPst(pstFilePath As String, pstName As String) As IEnumerable(Of MailItem)
+    '    Dim mailItems As New List(Of MailItem)()
+    '    Dim app As New Application()
+    '    Dim outlookNs As [NameSpace] = app.GetNamespace("MAPI")
+    '    ' Add PST file (Outlook Data File) to Default Profile
+    '    outlookNs.AddStore(pstFilePath)
+    '    Dim rootFolder As MAPIFolder = outlookNs.Stores(pstName).GetRootFolder()
+    '    ' Traverse through all folders in the PST file
+    '    ' TODO: This is not recursive, refactor
+    '    Dim subFolders As Folders = rootFolder.Folders
+    '    For Each folder As Folder In subFolders
+    '        Dim items As Items = folder.Items
+    '        For Each item As Object In items
+    '            If TypeOf item Is MailItem Then
+    '                Dim mailItem As MailItem = TryCast(item, MailItem)
+    '                mailItems.Add(mailItem)
+    '            End If
+    '        Next
+    '    Next
+    '    ' Remove PST file from Default Profile
+    '    outlookNs.RemoveStore(rootFolder)
+    '    Return mailItems
+    '    'End Function
+    '    ****write this code for getting the mails ***************
+
+    '    Dim objOL As Outlook.Application
+    '    Dim objNS As Outlook.NameSpace
+    '    Dim objFolder As Outlook.Folders
+    '    Dim Item As Object
+    '    Dim myItems As Outlook.Items
+    '    Dim x As Int16
+
+    'objOL = New Outlook.Application()
+    'objNS = objOL.GetNamespace("MAPI")
+
+    '    Dim olfolder As Outlook.MAPIFolder
+    'olfolder = objOL.GetNamespace("MAPI").PickFolder
+    'myItems = olfolder.Items
+
+    '    Dim i As Integer
+    'For x = 1 To myItems.Count
+    '    messagebox.show(myItems.Item(x).SenderName) 
+    '    messagebox.show myItems.Item(x).SenderEmailAddress)
+    '    messagebox.showmyItems.Item(x).Subject)
+    '    messagebox.showmyItems.Item(x).Body)
+    '    messagebox.show myItems.Item(x).to)
+    '    messagebox.showmyItems.Item(x).ReceivedByName)
+    '    messagebox.show myItems.Item(x).ReceivedOnBehalfOfName)
+    '    messagebox.showmyItems.Item(x).ReplyRecipientNames)
+    '    messagebox.showmyItems.Item(x).SentOnBehalfOfName)
+    '    messagebox.showmyItems.Item(x).CC)
+    '    messagebox.show myItems.Item(x).ReceivedTime)
+    'Next x
+
+    '***********for getting attachments use this code******************
+
+    '    Dim Atmt As Outlook.Attachment
+
+    'For Each Atmt In Item.Attachments
+    '    filename = "C:\Email Attachments\" + Atmt.FileName
+    '    Atmt.SaveAsFile(filename)
+    'Next Atmt
+
+    Private Function ExtractEmailAddressesFromString(ByVal source As String) As String()
+        Dim mc As MatchCollection
+        Dim i As Integer
+
+        ' expression garnered from www.regexlib.com - thanks guys!
+        mc = Regex.Matches(source, _
+            "([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})")
+        Dim results(mc.Count - 1) As String
+        For i = 0 To results.Length - 1
+            results(i) = mc(i).Value
+        Next
+
+        Return results
+    End Function
+
+    Private Sub RUN_READ(ByVal processoAtivo As BackgroundWorker)
+    
+        Dim objOL As Application
+        Dim objNS As Outlook.NameSpace
+        Dim myItems As Items
+        Dim x As Integer
+
+        objOL = New Application()
+        objNS = objOL.GetNamespace("MAPI")
+
+        Dim olfolder As MAPIFolder
+        olfolder = objOL.GetNamespace("MAPI").PickFolder
+        myItems = olfolder.Items
+
+        Itens = myItems.Count
+
+        For x = 1 To myItems.Count
+
+            On Error Resume Next
+
+            If (processoAtivo.CancellationPending = True) Then Exit For
+
+            Threading.Thread.Sleep(50)
+            'processoAtivo.ReportProgress((x / myItems.Count) * 100)
+            processoAtivo.ReportProgress(x)
+
+            For Each I In ExtractEmailAddressesFromString(myItems.Item(x).SenderEmailAddress.ToString().Trim())
+                'Emails.Rows.Add(I.ToString())
+                Retorno.Add(I.ToString())
+            Next
+
+            For Each I In ExtractEmailAddressesFromString(myItems.Item(x).to.ToString().Trim())
+                'Emails.Rows.Add(I.ToString())
+                Retorno.Add(I.ToString())
+            Next
+
+            For Each I In ExtractEmailAddressesFromString(myItems.Item(x).CC.ToString().Trim())
+                'Emails.Rows.Add(I.ToString())
+                Retorno.Add(I.ToString())
+            Next
+
+            For Each I In ExtractEmailAddressesFromString(myItems.Item(x).Body.ToString().Trim())
+                'Emails.Rows.Add(I.ToString())
+                Retorno.Add(I.ToString())
+            Next
+
+        Next x
+
+        ProcessoFinalizado = True
+        
+    End Sub
+
+    Private Sub ExeBTN_Click(sender As Object, e As EventArgs) Handles ExeBTN.Click
+
+        Contador = 0
+        CalculaTempo = 0
+        Timer1.Start()
+        ToolStripProgressBar1.Value = 0
+        ToolStripStatusLabel1.Text = "Extração . . . "
+        GeraExtracao
+
+    End Sub
+
+    Private Sub ExportBTN_Click(sender As Object, e As EventArgs) Handles ExportBTN.Click
+
+        Try
+            If Emails.Rows.Count > 0 Then
+
+                SaveFileDialog1.Filter = "Excel (*.csv) |*.csv"
+                SaveFileDialog1.ShowDialog()
+
+                Dim StrExport As String = ""
+
+                For Each C As DataGridViewColumn In Emails.Columns
+                    StrExport &= """" & C.HeaderText & ""","
+                Next
+                StrExport = StrExport.Substring(0, StrExport.Length - 1)
+                StrExport &= Environment.NewLine
+
+                For Each R As DataGridViewRow In Emails.Rows
+                    For Each C As DataGridViewCell In R.Cells
+                        If Not C.Value Is Nothing Then
+                            StrExport &= """" & C.Value.ToString.Trim() & ""","
+                        Else
+                            StrExport &= """" & "" & ""","
+                        End If
+                    Next
+                    StrExport = StrExport.Substring(0, StrExport.Length - 1)
+                    StrExport &= Environment.NewLine
+                Next
+
+                Dim tw As IO.TextWriter = New IO.StreamWriter(SaveFileDialog1.FileName)
+                tw.Write(StrExport)
+                tw.Close()
+                MsgBox("Operação Finalizada", , "Finalizado")
+            End If
+
+        Catch ex As System.Exception
+            MsgBox(ex.Message)
+        End Try
+        
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker.DoWork
+
+        Try
+            Dim segundoPlano As BackgroundWorker
+            segundoPlano = CType(sender, BackgroundWorker)
+            RUN_READ(segundoPlano)
+            If (BackgroundWorker.CancellationPending = True) Then e.Cancel = True
+
+        Catch ex As System.Exception
+            ProcessoFinalizado = False
+            MessageBox.Show(ex.Message, "Alerta de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End Try
+
+    End Sub
+
+    Private Sub GeraExtracao()
+
+        Try
+            ExeBTN.Enabled = False
+            ExportBTN.Enabled = False
+            Emails.Enabled = False
+            CancBTN.Visible = True
+            Me.BackgroundWorker.RunWorkerAsync()
+
+        Catch ex As System.Exception
+            ExeBTN.Enabled = True
+            ExportBTN.Enabled = True
+            Emails.Enabled = True
+            CancBTN.Visible = False
+            MessageBox.Show(ex.Message, "Alerta de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End Try
+
+    End Sub
+
+    Private Sub BackgroundWorker_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles BackgroundWorker.ProgressChanged
+        
+        If CalculaTempo = 2 Then
+            Contador += 1
+            calc = (((CalculaTempo * Contador) * Itens) / e.ProgressPercentage) / 60
+            CalculaTempo = 0
+        End If
+
+        ToolStripProgressBar1.Maximum = Itens
+        ToolStripProgressBar1.Value = e.ProgressPercentage
+        ToolStripStatusLabel2.Text = e.ProgressPercentage & " de " & Itens & " . . . Tempo Estmiado: " & calc & " min(s)"
+
+    End Sub
+    Private Sub BackgroundWorker_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker.RunWorkerCompleted
+
+
+        For Each i In Retorno
+            Dim valida As Boolean = True
+            If Emails.RowCount > 0 Then
+                For Each row As DataGridViewRow In Emails.Rows
+                    If row.Cells(0).Value IsNot Nothing AndAlso row.Cells(0).Value.ToString.Trim() = i.ToString.Trim() Then
+                        valida = False
+                    End If
+                Next
+            End If
+            If valida = True Then Emails.Rows.Add((i.ToString()))
+        Next
+
+        If (e.Cancelled = True) Then
+            ToolStripStatusLabel1.Text = "Extração Cancelada!"
+        Else
+            If ProcessoFinalizado = True Then
+                ToolStripStatusLabel1.Text = "Extração Finalizada!"
+
+            Else
+                ToolStripStatusLabel1.Text = "Extração com Erro!"
+            End If
+        End If
+
+        ToolStripProgressBar1.Value = Itens
+        ToolStripStatusLabel2.Text = String.Empty
+        ExeBTN.Enabled = True
+        Emails.Enabled = True
+        CancBTN.Visible = False
+        ExportBTN.Enabled = True
+
+    End Sub
+
+    Private Sub CancBTN_Click(sender As Object, e As EventArgs) Handles CancBTN.Click
+        BackgroundWorker.CancelAsync()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        
+        CalculaTempo += 1
+
+    End Sub
+
+End Class
